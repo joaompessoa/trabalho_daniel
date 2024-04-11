757d23fa1d7c3b4b1aea6b7210e17e3d471c53e1
Joao Severino <joaoseverino@Joaos-MacBook-Pro.local>
2024-04-11 16:48:30 -0300

    added some data

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def recursive_search(file, count=0):
     return recursive_search(file, count)
 
 
-def count_season_2(csv_file):
+def contar_monolitico(csv_file):
     count = 0
     with open(csv_file, 'r') as file:
         headers = next(file).strip().split(',')
@@ -37,14 +37,63 @@ def count_season_2(csv_file):
             columns = line.strip().split(',')
             title = columns[title_index]
             if re.search(r'Season 2', title, re.IGNORECASE):  # Busca por 'Season 2' ignorando maiúsculas e minúsculas
-                count += 1
+                count = count +  1
     return count
 
+def contar_interativo(csv_file,coluna,palavra):
+    def definir_colunas(csv_file):
+        hearders = next(csv_file).strip().split(',')
+        return hearders
+    def achar_index_coluna(headers,coluna):
+        title_index = headers.index(coluna)
+        return title_index 
+    def contador(palavra,coluna):
+        count = 0
+        with open(csv_file, 'r') as file:
+            headers = definir_colunas(file)
+            title_index = achar_index_coluna(headers,coluna)
+            for line in file:
+                colums = line.strip().split(',')
+                title = colums[title_index]
+                if re.search(fr'{palavra}', title, re.IGNORECASE):  # Busca por 'Season 2' ignorando maiúsculas e minúsculas
+                    count += 1
+        return count
+    return contador(palavra,coluna)
+
+def contar_recursivo(csv_file, coluna, palavra):
+    def definir_colunas(file):
+        headers = next(file).strip().split(',')
+        return headers
+    
+    def achar_index_coluna(headers, coluna):
+        title_index = headers.index(coluna)
+        return title_index
+    
+    def ler_linha(file, title_index, palavra, count=0):
+        line = file.readline()
+        if not line:
+            return count
+        columns = line.strip().split(',')
+        title = columns[title_index]
+        if re.search(fr'{palavra}', title, re.IGNORECASE):
+            count += 1
+        return ler_linha(file, title_index, palavra, count)
+    
+    with open(csv_file, 'r') as file:
+        headers = definir_colunas(file)
+        title_index = achar_index_coluna(headers, coluna)
+        count = ler_linha(file, title_index, palavra)
+        return count
 
 # Carregar dados e aplicar a função
 if __name__ == "__main__":
-    number = count_season_2('main_Full_data.csv')
+    dados = 'backup.csv'
+    coluna = 'id'
+    palavra = '2'
+    total = contar_interativo('backup.csv','id','2')
+    total_recursivo = contar_recursivo(dados,coluna,palavra)
 
-    print(number)
+    print(f"Numero de entradas em {dados} que contem a palavra: {palavra} na coluna: {coluna} e: {total}")
+    print(total_recursivo)
     
 
