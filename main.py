8534f97efd59741b377c5f33168f75b61a3f9323
Joao Severino <joaoseverino@Joaos-MacBook-Pro.local>
2024-04-11 16:15:46 -0300

    added the test code

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ def monolithic_search(csv_file):
                 count += 1
     return count
 
-# Carregar dados e aplicar a função
-result = monolithic_search('animes.csv')
-print("Número de animes com 'Season 2':", result)
-
 def iterative_search(csv_file):
     count = 0
     with open(csv_file, 'r') as file:
@@ -22,11 +18,6 @@ def iterative_search(csv_file):
                 count += 1
     return count
 
-# Carregar dados e aplicar a função
-result = iterative_search('animes.csv')
-print("Número de animes com 'Season 2':", result)
-
-
 def recursive_search(file, count=0):
     line = file.readline()
     if not line:
@@ -35,13 +26,25 @@ def recursive_search(file, count=0):
         count += 1
     return recursive_search(file, count)
 
-def main(csv_file):
+
+def count_season_2(csv_file):
+    count = 0
     with open(csv_file, 'r') as file:
-        next(file)  # Pula o cabeçalho
-        result = recursive_search(file)
-    return result
+        headers = next(file).strip().split(',')
+        title_index = headers.index('title')  # Encontra o índice da coluna 'title'
+        
+        for line in file:
+            columns = line.strip().split(',')
+            title = columns[title_index]
+            if re.search(r'Season 2', title, re.IGNORECASE):  # Busca por 'Season 2' ignorando maiúsculas e minúsculas
+                count += 1
+    return count
+
 
 # Carregar dados e aplicar a função
 if __name__ == "__main__":
+    number = count_season_2('main_Full_data.csv')
+
+    print(number)
     
 
